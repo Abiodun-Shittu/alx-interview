fd29767d642f2cded95049e80bdce57c64915861
Abiodun Shittu <jerrywizklay1@gmail.com>
2023-05-04 22:14:50 +0100

    Minimum Operations

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -8,7 +8,7 @@ needed to result in exactly n H characters in the file.
 def minOperations(n):
     if n == 1:
         return 0
-    
+
     ops = 0
     h = 1
     while h < n:
@@ -17,7 +17,7 @@ def minOperations(n):
             ops += 1
         h += h_copy
         ops += 1
-        
+
     if h == n:
         return ops
     else:
